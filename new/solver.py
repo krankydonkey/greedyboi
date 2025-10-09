035d5fd8ad86d3b63987d2ac7b42ba82feb27ed9
Damian van Kranendonk <dvankran@gmail.com>
2025-10-10 05:46:20 +1000

    added solver classes

--- new/solver.py
+++ new/solver.py
@@ -12,10 +12,15 @@ class SolvedState:
     def best_move(self, roll: tuple[int]) -> SolvedState | None:
         return self.decisions.get(roll)
     
+    def expected_value(self) -> float:
+        return 0.0
+
+    
 def play(zero: SolvedState):
     score = 0
     while True:
         state = zero
+        print(f"Total score: {score}")
         while True:
             print(f"Current state: {state.dice}, score: {state.score}, reroll: {state.reroll}")
             dice = input("Enter rolled dice: ") # do parsing to get in tuple form
@@ -25,5 +30,46 @@ def play(zero: SolvedState):
             elif dice in state.decisions:
                 state = dice
             else:
+                print("Ruh roh, no scoring dice found.")
                 break
+
+
+@dataclass
+class Edge:
+    start: State
+    end: State
+    options: set[State]
+    odds: float
+    ready: bool = False
+
+
+
+
+@dataclass
+class State:
+    dice: tuple[int]
+    score: int
+    greater: set[Edge]
+    less: set[Edge]
+    value: int
+    ready: bool = False
+
+
+
+@dataclass
+class Solver:
+    total_dice: int
+    num_faces: int
+    scoring_dice: dict[tuple[int], int]
+    states: set[State]
+    start_state: State
+    end_states: set[State]
+
+
         
+
+current = 500
+expected_value = 100
+odds = 0.6
+
+reroll = expected_value * odds > current
