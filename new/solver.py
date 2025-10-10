22bc48849e86546578d79f14002eb7dd7efb38df
Damian van Kranendonk <dvankran@gmail.com>
2025-10-11 07:58:02 +1000

    something

--- new/solver.py
+++ new/solver.py
@@ -3,25 +3,48 @@ from dataclasses import dataclass, field
 import numpy as np
 from scipy.special import factorial
 
+# In the following functions, the arrays have three extra columns.
+# The first is the score, second is the corresponding index in the state list,
+# and the thirs is the corresponding index in the most recent probability list
+
+def get_dice(rolls: np.ndarray):
+    return rolls[:,:-3] if rolls.ndim > 1 else rolls[:-3]
+
+def get_scores(rolls: np.ndarray):
+    return rolls[:,-3] if rolls.ndim > 1 else rolls[-3]
+
+def get_state_indices(rolls: np.ndarray):
+    return rolls[:,-2] if rolls.ndim > 1 else rolls[-2]
+
+def get_odds_indices(rolls: np.ndarray):
+    return rolls[:,-1] if rolls.ndim > 1 else rolls[-1]
+
+def set_odds_indices(rolls: np.ndarray, indices):
+    if rolls.ndim > 1:
+        rolls[:,-1] = indices 
+    else:
+        rolls[-1] = indices
+
 
 def get_roll_odds(num_dice: int, num_faces: int, rolls: np.ndarray):
-    face_odds = np.full(rolls.shape[1], 1 / num_faces)
-    return factorial(num_dice) * np.prod(np.power(face_odds, rolls), axis=1) / np.prod(factorial(rolls), axis=1)
+    dice = get_dice(rolls)
+    face_odds = np.full(dice.shape[1], 1 / num_faces)
+    return factorial(num_dice) * np.prod(np.power(face_odds, dice), axis=1) / np.prod(factorial(dice), axis=1)
 
 
-def get_greater_rows(matrix: np.ndarray, row: np.ndarray) -> tuple[np.ndarray, np.ndarray]:
-    greater_equal_indices = (matrix >= row).all(axis=1).nonzero()[0]
-    greater_equal_rows = matrix[greater_equal_indices]
-    greater_indices = (greater_equal_rows > row).any(axis=1).nonzero()[0]
-    return greater_equal_rows[greater_indices], greater_indices
+def get_greater_rolls(matrix: np.ndarray, roll: np.ndarray) -> tuple[np.ndarray, np.ndarray]:
+    greater_equal_indices = (get_dice(matrix) >= get_dice(roll)).all(axis=1).nonzero()[0]
+    greater_equal_rolls = matrix[greater_equal_indices]
+    greater_indices = (get_dice(greater_equal_rolls) > get_dice(roll)).any(axis=1).nonzero()[0]
+    return greater_equal_rolls[greater_indices], greater_indices
 
 
-def get_lesser_rows(matrix: np.ndarray, row: np.ndarray) -> tuple[np.ndarray, np.ndarray]:
-    lesser_equal_indices = (matrix <= row).all(axis=1).nonzero()[0]
-    lesser_equal_rows = matrix[lesser_equal_indices]
-    lesser_indices = (lesser_equal_rows < row).any(axis=1).nonzero()[0]
-    return lesser_equal_rows[lesser_indices], lesser_indices
-
+def get_lesser_rolls(matrix: np.ndarray, roll: np.ndarray) -> tuple[np.ndarray, np.ndarray]:
+    lesser_equal_indices = (get_dice(matrix) <= get_dice(roll)).all(axis=1).nonzero()[0]
+    lesser_equal_rolls = matrix[lesser_equal_indices]
+    lesser_indices = (get_dice(lesser_equal_rolls) < get_dice(roll)).any(axis=1).nonzero()[0]
+    return lesser_equal_rolls[lesser_indices], lesser_indices
+        
 
 @dataclass
 class SolvedState:
@@ -104,47 +127,39 @@ class Solver:
             max_index = np.where(mask)[0][max_mask]
             to_keep.append(max_index)
         self.combo_scores = combo_scores[to_keep]
+        indices = np.arange(self.combo_scores.shape[0], dtype=int).reshape(-1,1)
+        zeros = np.zeros(self.combo_scores.shape[0], dtype=int).reshape(-1,1)
+        self.combo_scores = np.column_stack((self.combo_scores, indices, zeros))
 
 
     def populate(self):
         for combo in self.combo_scores:
-            state = State(tuple(combo[:-1]), int(combo[-1]))
+            state = State(tuple(get_dice(combo)), int(get_scores(combo)))
             self.states.append(state)
     
 
     def link(self):
-        rolls = self.combo_scores[:,:-1]
-        scores = self.combo_scores[:,-1]
-        for i in range(self.combo_scores.shape[0]):
-            roll = rolls[i]
-            num_dice = np.sum(roll)
-            greater_rolls, greater_indices = get_greater_rows(rolls, roll)
+        for roll in self.combo_scores:
+            num_dice = np.sum(roll[:-3])
+            greater_rolls, greater_indices = get_greater_rolls(self.combo_scores, roll)
             odds = get_roll_odds(num_dice, self.num_faces, greater_rolls - roll)
-            for j in range(greater_indices.size):
-                # Find options
-                greater_index = greater_indices[j]
-                greater_roll = greater_rolls[j]
-                _, option_indices = get_lesser_rows(greater_rolls, greater_roll)
-                option_score_indices = greater_indices[option_indices]
+            odds_indices = np.arange(odds.size, dtype=int)
+            set_odds_indices(greater_rolls, odds_indices)
 
-                # Find immediately greater dice
-                greatest_rolls, greatest_indices = get_greater_rows(greater_rolls, greater_roll)
-
-                one_greater_indices = []
-                for k in greatest_indices:
-                    greatest_roll = self.combo_scores[:,:-1][k]
-                    _, lesser_indices = get_lesser_rows(greatest_rolls, greatest_roll)
-                    lesser_indices = greatest_indices[lesser_indices]
-
+            for greater_roll in greater_rolls:
+                middle_rolls, middle_indices = get_lesser_rolls(greater_rolls, greater_roll)
+                greatest_rolls, greatest_indices = get_greater_rolls(greater_rolls, greater_roll)
+                one_greater = []
+                for greatest_roll in greatest_rolls:
+                    _, lesser_indices = get_lesser_rolls(greatest_rolls, greatest_roll)
                     if lesser_indices.size == 0:
-                        one_greater_indices.append(k)
+                        one_greater.append(get_odds_indices(greatest_roll))
+                greater_odds = odds[get_odds_indices(greater_roll)] - np.sum(odds[one_greater])
 
-                greater_odds = odds[j] - np.sum(odds[one_greater_indices])
-                
                 edge = Edge(
-                    self.states[i],
-                    self.states[greater_index],
-                    [self.states[k] for k in option_score_indices],
+                    self.states[get_state_indices(roll)],
+                    self.states[get_state_indices(greater_roll)],
+                    [self.states[k] for k in get_state_indices(middle_rolls)],
                     greater_odds
                 )
                 edge.start.greater.append(edge)
